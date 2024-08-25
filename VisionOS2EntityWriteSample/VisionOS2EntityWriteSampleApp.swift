11983ffb5c6b596e197e52983135477809e29fed
tokufxug <tokufxug@gmail.com>
2024-08-25 22:17:13 +0900

    Initial Commit

---
+++ VisionOS2EntityWriteSample/VisionOS2EntityWriteSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  VisionOS2EntityWriteSampleApp.swift
+//  VisionOS2EntityWriteSample
+//
+//  Created by Sadao Tokuyama on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOS2EntityWriteSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
---
+++ VisionOS2EntityWriteSampleTests/VisionOS2EntityWriteSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOS2EntityWriteSampleTests.swift
+//  VisionOS2EntityWriteSampleTests
+//
+//  Created by Sadao Tokuyama on 8/25/24.
+//
+
+import Testing
+@testable import VisionOS2EntityWriteSample
+
+struct VisionOS2EntityWriteSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
